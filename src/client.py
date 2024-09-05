264c8affd6097dba702405aa48cf51d013a82616
Rhuan Nascimento <rhuan2414@gmail.com>
2024-09-05 20:45:31 -0300

    progresso na conexao e enviar por fila

--- src/client.py
+++ src/client.py
@@ -1,43 +1,25 @@
 import socket
-import wave
-import json
+import socket
 
 # Configurações do cliente
 IP_SERVIDOR = "127.0.0.1"
 PORTA_SERVIDOR = 12345
 PORTA_CLIENTE = 54321
-TAMANHO_BUFFER = 4096  # Aumentado para 4096 bytes
+TAMANHO_BUFFER = 2048 * 2
 
 def cliente_udp():
     cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    cliente_socket.bind(("0.0.0.0", PORTA_CLIENTE))
-
-    print(f"Cliente ouvindo na porta {PORTA_CLIENTE}")
-
-    # Recebe a primeira mensagem que serão os metadados
-    metadados, _ = cliente_socket.recvfrom(TAMANHO_BUFFER)
-    
-    # Verifica se a mensagem começa com a tag "META"
-    if metadados.decode().startswith("META"):
-        # Remove a tag "META" e converte para dicionário
-        metadados = json.loads(metadados.decode()[4:])
-    else:
-        print("Erro: não foram recebidos metadados válidos.")
-        return
-
-    # Cria arquivo de saída para salvar o áudio
-    with wave.open(f'output_cliente_{PORTA_CLIENTE}.wav', 'wb') as wav_file:
-        wav_file.setnchannels(metadados['nchannels'])
-        wav_file.setsampwidth(metadados['sampwidth'])
-        wav_file.setframerate(metadados['framerate'])
-
+    cliente_socket.sendto(b"opa", (IP_SERVIDOR, PORTA_SERVIDOR))
+   
+    try:
         while True:
-            dados, _ = cliente_socket.recvfrom(TAMANHO_BUFFER)
-            if not dados:
-                break
-            wav_file.writeframes(dados)
+            audio_data, _ = cliente_socket.recvfrom(TAMANHO_BUFFER)
+            print(audio_data)
+    finally:
+        cliente_socket.close()
 
-    cliente_socket.close()
-    print(f"Áudio recebido e salvo como output_cliente_{PORTA_CLIENTE}.wav")
 
 cliente_udp()
+
+
+
--- src/server.py
+++ src/server.py
@@ -1,12 +1,26 @@
 import socket
 import wave
+import queue
+import threading
 import time
-import json
 
 # Configurações do servidor
 IP_SERVIDOR = "0.0.0.0"
 PORTA_SERVIDOR = 12345
-TAMANHO_BUFFER = 1024  # Tamanho do bloco de dados enviado a cada pacote
+TAMANHO_BUFFER = 1024
+TIMEOUT_ACK = 5  # Tempo máximo de espera por ACK em segundos
+
+audio_queue = queue.Queue()
+
+def handle_client(client_address, server_socket): 
+    while True:
+        try:
+            data = audio_queue.get(timeout=1)
+            if(data):
+                server_socket.sendto(data, client_address)
+        except queue.Empty:
+            time.sleep(1)
+
 
 def servidor_udp(caminho_arquivo_wav):
     with wave.open(caminho_arquivo_wav, 'rb') as wav_file:
@@ -15,38 +29,26 @@ def servidor_udp(caminho_arquivo_wav):
         
         print(f"Servidor ouvindo em {IP_SERVIDOR}:{PORTA_SERVIDOR}")
 
-        # Metadados
-        metadados = {
-            'nchannels': wav_file.getnchannels(),
-            'sampwidth': wav_file.getsampwidth(),
-            'framerate': wav_file.getframerate(),
-            'nframes': wav_file.getnframes()
-        }
-        
-        # Compacta os metadados usando JSON e adiciona uma tag especial "META"
-        metadados_json = "META" + json.dumps(metadados)
-        
-        for cliente in lista_clientes:
-            servidor_socket.sendto(metadados_json.encode(), cliente)
-        
-        # Transmissão de áudio
         while True:
             dados = wav_file.readframes(TAMANHO_BUFFER)
+            audio_queue.put(dados)
             if not dados:
                 break
+            
+            _, client_address = servidor_socket.recvfrom(TAMANHO_BUFFER)
+            client_thread = threading.Thread(target=handle_client, args=(client_address, servidor_socket))
+            client_thread.start()
 
-            for cliente in lista_clientes:
-                servidor_socket.sendto(dados, cliente)
-
-            time.sleep(0.01)
+            time.sleep(0.01)  # Pequeno atraso para evitar sobrecarga de pacotes
 
     servidor_socket.close()
     print("Transmissão concluída.")
 
-# Lista de clientes
-lista_clientes = [("127.0.0.1", 54321)]
 
 # Caminho do arquivo .wav
 caminho_arquivo = "vasco.wav"
 
 servidor_udp(caminho_arquivo)
+
+
+
