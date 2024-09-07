708449d13db0aa1c49658b3bdd7835b47c2a2e16
Rhuan Nascimento Ferreira <rhuan2414@gmail.com>
2024-09-07 19:05:10 -0300

    Sistema de salas

--- src/server.py
+++ src/server.py
@@ -1,29 +1,59 @@
 import socket
-import pyaudio
+import threading
 
-CHUNK = 1024  
-FORMAT = pyaudio.paInt16 
-CHANNELS = 1 
-RATE = 44100 
+CHUNK = 8192 * 2
 
-audio = pyaudio.PyAudio()
-stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+class AudioServer:
+    def __init__(self, host='0.0.0.0', port=5005):
+        self.host = host
+        self.port = port
+        self.salas = {}  # Dicionário para armazenar salas e seus clientes
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.host, self.port))
 
-UDP_IP = '127.0.0.255'
-UDP_PORT = 5005
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    def criar_sala(self, nome_sala, addr):
+        if nome_sala not in self.salas:
+            self.salas[nome_sala] = []
+            print(f"Sala '{nome_sala}' criada por {addr}")
+        else:
+            print(f"Sala '{nome_sala}' já existe.")
 
-print("Transmitindo áudio...")
+    def adicionar_cliente(self, nome_sala, addr):
+        if nome_sala in self.salas:
+            self.salas[nome_sala].append(addr)
+            print(f"Cliente {addr} entrou na sala '{nome_sala}'")
 
-try:
-    while True:
-        data = stream.read(CHUNK)
-        sock.sendto(data, (UDP_IP, UDP_PORT))
-except KeyboardInterrupt:
-    print("Transmissão encerrada.")
-finally:
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-    sock.close()
+    def distribuir_audio(self, nome_sala, data, addr_origem):
+        if nome_sala in self.salas:
+            for addr in self.salas[nome_sala]:
+                if addr != addr_origem: 
+                    print(addr)
+                    print(addr_origem)
+                    self.server_socket.sendto(data, addr)
+
+
+
+    def handle_client(self):
+        while True:
+            data, addr = self.server_socket.recvfrom(CHUNK)
+            dataType, data = data.split()
+            dataType = dataType.decode()
+
+
+            if dataType.startswith("CRIAR"):
+                nome_sala = data.decode()
+                self.criar_sala(nome_sala, addr)
+            elif dataType.startswith("ENTRAR"):
+                nome_sala = data.decode()
+                self.adicionar_cliente(nome_sala, addr)
+            else:
+                nome_sala = dataType.decode()
+                self.distribuir_audio(nome_sala.decode(), data, addr)
+    
+    def iniciar(self):
+        print("Servidor central iniciado...")
+        threading.Thread(target=self.handle_client).start()
+
+servidor = AudioServer()
+servidor.iniciar()
